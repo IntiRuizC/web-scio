a96686a10e588103a34ef40b4339006742a80931
Inti <ilruizc@unal.edu.co>
2025-03-10 12:09:20 -0500

    Se definen valores de duración de scrollTrigger

--- src/components/productos.jsx
+++ src/components/productos.jsx
@@ -73,41 +73,41 @@ const ProductosScroll = () => {
 
         window.addEventListener('resize', handleResize);
 
-        // GSAP animations
         ScrollTrigger.create({
             trigger: ".productos-container",
             start: "top top",
-            end: "+=1000vh",
+            end: "+=1250vh",
             pin: true,
             scrub: 2,
         });
 
         gsap.to(".productos", {
-            xPercent: -66,
+            xPercent: -50,
             ease: "none",
             scrollTrigger: {
                 trigger: ".productos",
                 start: "center center+=10%",
-                end: "+=1000vh",
+                end: "+=1250vh",
                 scrub: 3,
+                markers: true
             },
         });
 
         let tl = gsap.timeline({
             scrollTrigger: {
                 trigger: ".productos",
-                start: "top-=200vh top+=150vh",
-                end: "bottom bottom",
-                markers: true,
+                start: "top-=200vh top",
+                end: "bottom top",
                 scrub: 2,
+                markers:false
             },
         });
 
-        tl.fromTo(".img-pro1",
+        tl.fromTo(".img-pro2",
             { y: -150, opacity: 0 },
-            { y: -10, opacity: 1, duration: 1 }
+            { y: -10, opacity: 1, duration: 0.5 }
         )
-            .fromTo(".img-pro2",
+            .fromTo(".img-pro1",
                 { y: -150, opacity: 0 },
                 { y: -10, opacity: 1, duration: 1 }
             );
