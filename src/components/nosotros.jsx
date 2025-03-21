11de309092ba7e063526c7b6ab5373b63bbe93b1
Jhon Alexaxnder González Ramírez <jgonzalez@sciocorp.org>
2025-03-21 16:17:39 -0500

    Grilla aplcada a nosotros, en versión mobile y pc en la sección value, con el respectivo cambio en los valores para la animación

--- src/components/nosotros.jsx
+++ src/components/nosotros.jsx
@@ -19,14 +19,14 @@ export default function ScrollAnimation() {
                 end: "+=100", // Altura completa de la sección
                 scrub: 2,
                 pin: true,
-                anticipatePin: 1
+                anticipatePin: 1,
             }
         });
 
         masterTL.fromTo(".text-section",
             { x: "100%", opacity: 0 },
-            { 
-                x: "0%", 
+            {
+                x: "0%",
                 opacity: 1,
                 ease: "power2.inOut"
             }
@@ -38,7 +38,7 @@ export default function ScrollAnimation() {
     // Animación de la barra de progreso
     useEffect(() => {
         const progressBar = lineRef.current;
-        
+
         gsap.fromTo(progressBar,
             { height: "0%" },
             {
@@ -59,40 +59,39 @@ export default function ScrollAnimation() {
 
 
     useEffect(() => {
-        // Animación para todos los elementos .value
-        gsap.utils.toArray(".value").forEach((value, i) => {
-            const h3 = value.querySelector("h3");
+        gsap.utils.toArray(".val-description").forEach((value, i) => {
+            const subtittle = value.previousElementSibling?.previousElementSibling?.querySelector("h3");
             const p = value.querySelector("p");
-            const subBall = value.querySelector(".sub-ball");
+            const subBall = value.previousElementSibling.querySelector(".sub-ball");
 
             // Timeline para cada elemento
             const tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: value,
-                    start:  "top+=20 center",
-                    end:  "bottom+=20 center",
+                    start: "top+=20 center",
+                    end: "bottom+=20 center",
+                    markers: true,
                     scrub: 1,
                 }
             });
 
-            tl.fromTo(h3,
-                { opacity: 0.3, y: 20 },
-                { opacity: 1, y: 0, color: "#036E7C" }
-            );
-    
-            // Animación para el p, sincronizada con el h3
             tl.fromTo(p,
                 { opacity: 0.3, y: 20 },
-                { opacity: 1, y: 0, color: "#f7eeba" }, // Nuevo color para el p
-                "<" // Esto asegura que ambas animaciones inicien al mismo tiempo
+                { opacity: 1, y: 0, color: "#f7eeba" },
+                "<"
             );
 
             // Animación para la bolita
             tl.fromTo(subBall,
                 { backgroundColor: "#cccccc" },
                 { backgroundColor: "#036E7C" },
-                "-=0.5"
-            );
+                "<"
+            )
+            tl.fromTo(subtittle,
+                { opacity: 0.3, y: 20 },
+                { opacity: 1, y: 0, color: "#036E7C" },
+                "<"
+            );;
         });
 
         // Limpieza
@@ -101,17 +100,17 @@ export default function ScrollAnimation() {
 
     return (
         <div className="nosotros" id="nosotros">
-            
+
             <div className="text-section" ref={textContainerRef}>
                 <p>
-                En SCIO convertimos <span>datos</span> en historias visuales que potencian decisiones estratégicas. 
-                Nos especializamos en organizar, normalizar, analizar y visualizar información para maximizar 
-                el valor de los <span>datos</span>. Nuestra fortaleza radica en combinar análisis preciso con diseño intuitivo. 
-                Colaboramos con universidades, empresas y entidades públicas, ofreciendo soluciones personalizadas 
-                que abarcan desde catalogación de <span>datos</span> hasta visualizaciones interactivas y recursos pedagógicos. 
-                Cada proyecto busca democratizar el conocimiento, transformando información compleja en herramientas 
-                accesibles. Porque los <span>datos</span> no son un fin, sino un punto de partida para hacer preguntas más idóneas 
-                y construir respuestas más audaces
+                    En SCIO convertimos <span>datos</span> en historias visuales que potencian decisiones estratégicas.
+                    Nos especializamos en organizar, normalizar, analizar y visualizar información para maximizar
+                    el valor de los <span>datos</span>. Nuestra fortaleza radica en combinar análisis preciso con diseño intuitivo.
+                    Colaboramos con universidades, empresas y entidades públicas, ofreciendo soluciones personalizadas
+                    que abarcan desde catalogación de <span>datos</span> hasta visualizaciones interactivas y recursos pedagógicos.
+                    Cada proyecto busca democratizar el conocimiento, transformando información compleja en herramientas
+                    accesibles. Porque los <span>datos</span> no son un fin, sino un punto de partida para hacer preguntas más idóneas
+                    y construir respuestas más audaces
                 </p>
             </div>
 
@@ -119,93 +118,89 @@ export default function ScrollAnimation() {
 
             <div className="values-section" ref={valuesRef}>
                 <h2>VALORES</h2>
-                <div className="line-container">
-                    <div className="progress">
-                        <div className="line-background">
-                            <div className="line-progress" ref={lineRef}></div>
+
+
+                <div className="values">
+                    <div className="subtittle escep">
+                        <h3>ESCEPTICISMO</h3>
+                    </div>
+                    <div className="ball-contain escep">
+                        <div className="sub-ball"></div>
+                    </div>
+                    <div className="val-description escep">
+                        <p>
+                            Cuestionar y verificar la información antes de aceptarla, garantizando
+                            que los datos y análisis sean fiables y precisos. Esto respalda nuestra
+                            misión de ofrecer información clara y educativa basada en fundamentos
+                            sólidos.
+                        </p>
+                    </div>
+                    <div className="subtittle criticismo">
+                        <h3>CRITICISMO</h3>
+                    </div>
+                    <div className="ball-contain criticismo">
+                        <div className="sub-ball"></div>
+                    </div>
+                    <div className="val-description criticismo">
+                        <p>
+                            Fomentar una actitud crítica hacia nuestras soluciones para impulsar la
+                            innovación y la mejora continua, asegurando que nuestra misión de
+                            transformar datos en información valiosa y educativa se mantenga en
+                            constante evolución
+                        </p>
+                    </div>
+
+                    <div className="line-container">
+                        <div className="progress">
+                            <div className="line-background">
+                                <div className="line-progress" ref={lineRef}></div>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="values">
-                        <div className="value">
-                            <div className="subtittle">
-                                <h3>ESCEPTICISMO</h3>
-                            </div>
-                            <div className="ball-contain">
-                                <div className="sub-ball"></div>
-                            </div>
-                            <div className="val-description">
-                                <p>
-                                    Cuestionar y verificar la información antes de aceptarla, garantizando
-                                    que los datos y análisis sean fiables y precisos. Esto respalda nuestra
-                                    misión de ofrecer información clara y educativa basada en fundamentos
-                                    sólidos.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value">
-                            <div className="subtittle">
-                                <h3>CRITICISMO</h3>
-                            </div>
-                            <div className="ball-contain">
-                                <div className="sub-ball"></div>
-                            </div>
-                            <div className="val-description">
-                                <p>
-                                    Fomentar una actitud crítica hacia nuestras soluciones para impulsar la
-                                    innovación y la mejora continua, asegurando que nuestra misión de
-                                    transformar datos en información valiosa y educativa se mantenga en
-                                    constante evolución
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value">
-                            <div className="subtittle">
-                                <h3>PESIMISMO TECNOLÓGICO</h3>
-                            </div>
-                            <div className="ball-contain">
-                                <div className="sub-ball"></div>
-                            </div>
-                            <div className="val-description">
-                                <p>
-                                    Adoptar una actitud crítica respecto a los posibles efectos adversos de la
-                                    tecnología, para equilibrar la innovación con una evaluación cuidadosa
-                                    de sus impactos, alineándonos con nuestra misión de ofrecer soluciones
-                                    sostenibles y efectivas.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value">
-                            <div className="subtittle">
-                                <h3>TRANS- <br /> DISCIPLINARIEDAD</h3>
-                            </div>
-                            <div className="ball-contain">
-                                <div className="sub-ball"></div>
-                            </div>
-                            <div className="val-description">
-                                <p>
-                                    Integrar conocimientos de diversas disciplinas para abordar problemas
-                                    complejos de manera efectiva, apoyando nuestra misión de simplificar y
-                                    educar sobre la información mediante un enfoque colaborativo y
-                                    holístico
-                                </p>
-                            </div>
-                        </div>
-                        <div className="value">
-                            <div className="subtittle">
-                                <h3>COMPETENCIA COMUNICATIVA</h3>
-                            </div>
-                            <div className="ball-contain">
-                                <div className="sub-ball"></div>
-                            </div>
-                            <div className="val-description">
-                                <p>
-                                    Contar con las facultades que nos permitan una buena interacción con
-                                    diferentes tipos de usuarios, con el fin de responder a sus necesidades
-                                    informacionales
-                                </p>
-                            </div>
-                        </div>
+
+                    <div className="subtittle pesimismo">
+                        <h3>PESIMISMO TECNOLÓGICO</h3>
+                    </div>
+                    <div className="ball-contain pesimismo">
+                        <div className="sub-ball"></div>
+                    </div>
+                    <div className="val-description pesimismo">
+                        <p>
+                            Adoptar una actitud crítica respecto a los posibles efectos adversos de la
+                            tecnología, para equilibrar la innovación con una evaluación cuidadosa
+                            de sus impactos, alineándonos con nuestra misión de ofrecer soluciones
+                            sostenibles y efectivas.
+                        </p>
+                    </div>
+
+                    <div className="subtittle trans">
+                        <h3>TRANS- <br /> DISCIPLINARIEDAD</h3>
+                    </div>
+                    <div className="ball-contain trans">
+                        <div className="sub-ball"></div>
+                    </div>
+                    <div className="val-description trans">
+                        <p>
+                            Integrar conocimientos de diversas disciplinas para abordar problemas
+                            complejos de manera efectiva, apoyando nuestra misión de simplificar y
+                            educar sobre la información mediante un enfoque colaborativo y
+                            holístico
+                        </p>
+                    </div>
+
+                    <div className="subtittle competencia">
+                        <h3>COMPETENCIA COMUNICATIVA</h3>
+                    </div>
+                    <div className="ball-contain competencia">
+                        <div className="sub-ball"></div>
+                    </div>
+                    <div className="val-description competencia">
+                        <p>
+                            Contar con las facultades que nos permitan una buena interacción con
+                            diferentes tipos de usuarios, con el fin de responder a sus necesidades
+                            informacionales
+                        </p>
                     </div>
                 </div>
             </div>
